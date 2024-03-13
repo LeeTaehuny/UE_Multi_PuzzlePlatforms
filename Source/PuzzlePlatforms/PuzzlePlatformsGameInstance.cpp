4c698ec96503c7ec1215aff64760139a04717457
LeeTaes <apth1023@naver.com>
2024-03-13 18:57:29 +0900

    콘솔 명령을 통한 서버 호스트 & 참여(완)

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+#include "Engine/Engine.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Start Construct"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Start Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	// AddOnScreenDebugMessage() 함수를 사용해 화면에 로그 띄우기
+
+	UEngine* Engine = GetEngine();
+	if (!Engine) return;
+
+	Engine->AddOnScreenDebugMessage(0, 1.5f, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+
+	if (!World) return;
+
+	World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	// AddOnScreenDebugMessage() 함수를 사용해 화면에 로그 띄우기
+
+	UEngine* Engine = GetEngine();
+	if (!Engine) return;
+
+	Engine->AddOnScreenDebugMessage(0, 1.5f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	// 플레이어 컨트롤러 받아오기
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+
+	if (!PlayerController) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformsGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPuzzlePlatformsGameInstance();
+
+	virtual void Init() override;
+
+public:
+	// 콘솔에서 사용하기 위한 함수 선언
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& Address);
+};
